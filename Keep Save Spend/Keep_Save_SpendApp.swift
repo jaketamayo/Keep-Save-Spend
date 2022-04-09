865700afbeeb64a42c40a6e1cea04e5815f05445
Jake Tamayo <hellojtmatthew@icloud.com>
2022-04-08 19:46:42 -0500

    Initial Commit

---
+++ Keep Save Spend/Keep_Save_SpendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keep_Save_SpendApp.swift
+//  Keep Save Spend
+//
+//  Created by Jacob Tamayo on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Keep_Save_SpendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
