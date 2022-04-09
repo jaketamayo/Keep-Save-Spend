e04934b9037b6f0a27c1378a52a0bae2871190d2
Jake Tamayo <hellojtmatthew@icloud.com>
2022-04-08 20:47:32 -0500

    Start of Keep, Save, Spend. Added a TextField and Button.

--- Keep Save Spend/ContentView.swift
+++ Keep Save Spend/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = ""
+    var fiftyPercent = 0.5
+    var thirtyPercent = 0.3
+    var twentyPercent = 0.2
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Enter Amount", text: $amount)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+            
+            Button("Calculate") {
+                var save = (Double(amount) ?? 0) * fiftyPercent
+            }
+        }
+        
+        .padding()
+     
     }
 }
 
