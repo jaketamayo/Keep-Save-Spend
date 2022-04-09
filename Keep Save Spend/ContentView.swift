a3b56a8644f4be730dd30eea267bc8dd510c509c
Jake Tamayo <hellojtmatthew@icloud.com>
2022-04-09 10:37:59 -0500

    Added a function that calculates 50%, 30%, and 20% of what the user types in

--- Keep Save Spend/ContentView.swift
+++ Keep Save Spend/ContentView.swift
@@ -9,24 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var amount = ""
-    var fiftyPercent = 0.5
-    var thirtyPercent = 0.3
-    var twentyPercent = 0.2
-
+    @State private var keep = ""
+    @State private var save = ""
+    @State private var spend = ""
     
     var body: some View {
         VStack {
             TextField("Enter Amount", text: $amount)
                 .textFieldStyle(.roundedBorder)
                 .keyboardType(.numberPad)
+            Button("Calculate"){
+                calculate()
+            }
+            .buttonStyle(.bordered)
             
-            Button("Calculate") {
-                var save = (Double(amount) ?? 0) * fiftyPercent
+            VStack {
+                Text("Keep: \(keep)")
+                    .padding()
+                Text("Save: \(save)")
+                    .padding()
+                Text("Spend: \(spend)")
+                    .padding()
             }
         }
         
         .padding()
      
+    }
+    
+    private func calculate() {
+        guard let value = Double(amount) else {
+            keep = String(0.0)
+            save = String(0.0)
+            spend = String(0.0)
+            return
+        }
+
+        let needPart = 0.5
+        let savePart = 0.3
+        let spendingPart = 0.2
+        
+        keep = "\(value * needPart)"
+        save = "\(value * savePart)"
+        spend = "\(value * spendingPart)"
     }
 }
 
